34314a3bed0122fdaace63d9b92c2595faa66819
akhmadzulfikarr <akhmadzulfikar1530@gmail.com>
2023-10-09 09:37:43 +0700

    dart

---
+++ lib/helper.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+late String? nama;
+void main() {
+  nama = stdin.readLineSync();
+  print("nama : $nama");
+
+  var apaAja = [1, 'a', true, null, 2.5, 0.001];
+  print(apaAja);
+
+  var a = [1, 2, 3, 4];
+  var b = [...a, 5];
+  print(b);
+
+  String? message;
+  String text = message ?? "Empty";
+
+  print(text);
+
+  List angka = [1, 2, 3, 4, 5];
+  printList();
+
+  angka.forEach((element) {
+    print(element);
+  });
+}
+
+void printList([List? l]) {
+  print('List l');
+} 
